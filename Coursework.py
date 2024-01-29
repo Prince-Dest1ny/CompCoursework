3926269bf5dba9834905cd21f2dd2687f580d07c
Prince-Dest1ny <100820991+Prince-Dest1ny@users.noreply.github.com>
2024-01-29 11:24:25 +0800

    s

--- Coursework.py
+++ Coursework.py
@@ -4,14 +4,13 @@ import streamlit as st
 
 class Budget():
     def __init__(self):
-        self.budget = 0
+        self.value = 0
         self.initialBudget = 0
         self.totalIncome = []
         self.totalExpense = []
         self.categories = {}
     
     def create(self):
-        self.budget = int(input("What's your budget? "))
         no = int(input("How many categories are there? "))
         for i in range(0,no):
             try:
@@ -23,16 +22,21 @@ class Budget():
             temp2 = int(input("What "))
 
     def income(self, inputAmt,date,source,recurring=0):
-        self.budget += inputAmt
+        self.value += inputAmt
         self.totalIncome.append([inputAmt,date,source,recurring])
     
     def expense(self, expenseAmt,date,source):
-        self.budget -= expenseAmt
+        self.value -= expenseAmt
         self.totalExpense.append([expenseAmt,date,source])
 
     #def Generation():
 
 
 st.title("Budgeteer")
+new = Budget()
+if st.button("New Category",type="primary"):
+    st.write('why')
+else:
+    st.write("")
 st.write()
     
