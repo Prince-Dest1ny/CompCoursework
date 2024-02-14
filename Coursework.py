626911135579396957c98b562ea3502070d1a332
enzr2 <157557493+enzr2@users.noreply.github.com>
2024-02-14 11:42:14 +0800

    Update Coursework.py

--- Coursework.py
+++ Coursework.py
@@ -77,7 +77,7 @@ def budgetPage():
                     st.warning("Amount inputted has exceeded budget!")
                 elif st.session_state.budgetLeft <= 0:
                     st.warning("Budget left has reached 0!")
-                elif st.session_state.budgetLeft - st.session_state.budgetValue = 0:
+                elif st.session_state.budgetLeft - st.session_state.budgetValue == 0:
                     data.budgetCost.append(budget_value)
                     data.budgetName.append(budget_name)
                 else:
